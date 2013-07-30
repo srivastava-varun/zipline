988f76eaa143c79c1ecc828af71c752b0828c7c9
Eddie Hebert <ehebert@quantopian.com>
2013-07-29 21:22:36 -0400

    More breakout of modules.

--- zipline/finance/risk/__init__.py
+++ zipline/finance/risk/__init__.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . risk import RiskReport
+from . report import RiskReport
 from . period import RiskMetricsPeriod
 from . cumulative import RiskMetricsCumulative
 
